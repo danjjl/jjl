947e02a01e241cb85b5cc7507ee51976e064415d
Jonathan Dan <danjjl@hotmail.com>
2013-11-16 19:52:56 +0100

    Display 403 error on wrong privileges

--- Peoulot/urls.py
+++ Peoulot/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('JJL.Peoulot.views',
     url(r'^$', login_required(ListPeoulot.as_view()), name='liste'),
     url(r'^toutes/$', login_required(ListPeoulot.as_view()), {'toutes' : True}, name='toutes'),
     url(r'^lire/(?P<pk>\d+)$', login_required(LirePeoula.as_view()), name='lire'),
-    url(r'^ajouter/$', permission_required('Peoulot.add_peoula')(ajouterModifier), name='ajouter'),
-    url(r'^modifier/(?P<pk>\d+)$', permission_required('Peoulot.change_peoula')(ajouterModifier), name='modifier'),
+    url(r'^ajouter/$', permission_required('Peoulot.add_peoula', raise_exception=True)(ajouterModifier), name='ajouter'),
+    url(r'^modifier/(?P<pk>\d+)$', permission_required('Peoulot.change_peoula', raise_exception=True)(ajouterModifier), name='modifier'),
 )
