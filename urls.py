29854953b23fa9eb012f119953ff173618e9c84a
Jonathan Dan <danjjl@hotmail.com>
2013-11-21 22:00:57 +0100

    Log In, mecanism now logges in already logged users.
    LogInTemplate no longuer inherits template view
    Fixed /login/ if called without ['mdp'] or ['nom']
    Mecanism would permit redirecting each user

--- urls.py
+++ urls.py
@@ -4,14 +4,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from JJL.Peoulot.views import ListPeoulot
-from JJL.views import login_view, logout_view, LoginTemplate
+from JJL.views import login_view, logout_view, home
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', LoginTemplate.as_view(), name='login'),
+    url(r'^$', home, name='login'),
     url(r'^login/$', login_view, name='validate'),
     url(r'^logout/$', logout_view, name='logout'),
     (r'^peoulot/', include('JJL.Peoulot.urls')),
--- views.py
+++ views.py
@@ -1,32 +1,28 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render_to_response
+from django.template import RequestContext
 from django.views.generic import TemplateView
 
-class LoginTemplate(TemplateView):
-    template_name='login.html'
-    #extra context
-    def get_context_data(self, **kwargs):
-        context = super(TemplateView, self).get_context_data(**kwargs)
-        context['pageTitle'] = 'Log In'
-        return context
+def home(request):
+    if request.user.is_authenticated():
+        return redirect('liste')
+    else:
+        return render_to_response('login.html', {
+            'pageTitle' : 'Log In',
+        }, context_instance=RequestContext(request))
 
 def login_view(request):
-    username = request.POST['nom']
-    password = request.POST['mdp']
+    username = request.POST.get('nom')
+    password = request.POST.get('mdp')
     user = authenticate(username=username, password=password)
-    if user is not None:
-        if user.is_active:
-            login(request, user)
-            messages.success(request, u'Tu as été connecté en tant que ' + username + u'. Bonnes péoulot.')
-            return redirect('liste')
-        else:
-            messages.warning(request, u'Ce compte a été désactivé. Il faudra se connecter avec un autre compte')
-            return redirect('login')
+    if user is not None and user.is_active:
+        login(request, user)
+        messages.success(request, u'Tu as été connecté en tant que ' + username + u'. Bonnes péoulot.')
     else:
         messages.warning(request, u'Oh :o Tu t\'es trompé dans le mot de passe ou dans le nom d\'utilisateur.')
-        return redirect('login')
+    return redirect('login')
 
 def logout_view(request):
     logout(request)
