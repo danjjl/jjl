9cd5979c53baaf3839bd72edc0d0d9674412189e
Jonathan Dan <danjjl@hotmail.com>
2013-12-29 17:52:14 +0100

    Fixed bug with age-kvoutsa
    After January ages wheren't changed eventhough year changed.

--- Kvoutsot/views.py
+++ Kvoutsot/views.py
@@ -4,12 +4,14 @@ from datetime import date
 from JJL.Kvoutsot.models import Kvoutsa
 
 #__FUNCTIONS__
-#retourne le nom de la kvoutsa actuelle ayant 'age'
+#retourne le nom de la kvoutsa actuelle ayant 'age' en septembre
 def nomKvoutsa(age):
     kvoutsa = ""
     try:
         #kvoutsot born on 01/09/year
-        kvoutsa = Kvoutsa.objects.get(date_creation=date(date.today().year + 6 - age, 9, 1))
+        if date.today().month < 8:
+            age += 1 #kvoutsa is older in january-august
+        kvoutsa = Kvoutsa.objects.get(date_creation=date(date.today().year + 7 - age, 9, 1))
     except ObjectDoesNotExist:
         #should notify admin
         pass
