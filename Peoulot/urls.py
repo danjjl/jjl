5d75cf1e5480b101a52a90c63d6c1514691ae0ca
Jonathan Dan <danjjl@hotmail.com>
2016-08-19 22:18:51 +0200

    Update urls to class based urlpatterns

--- Peoulot/urls.py
+++ Peoulot/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import *
+from django.conf.urls import url
 from django.contrib.auth.decorators import login_required, permission_required
 
 from JJL.Peoulot.views import ListPeoulot, LirePeoula, ajouterModifier, supprimerPeoula
 
-urlpatterns = patterns('JJL.Peoulot.views',
+urlpatterns = [
     url(r'^$', login_required(ListPeoulot.as_view()), name='liste'),
     url(r'^toutes/$', login_required(ListPeoulot.as_view()), {'toutes' : True}, name='toutes'),
     url(r'^lire/(?P<pk>\d+)$', login_required(LirePeoula.as_view()), name='lire'),
     url(r'^ajouter/$', permission_required('Peoulot.add_peoula', raise_exception=True)(ajouterModifier), name='ajouter'),
     url(r'^modifier/(?P<pk>\d+)$', permission_required('Peoulot.change_peoula', raise_exception=True)(ajouterModifier), name='modifier'),
     url(r'^supprimer/$', permission_required('Peoulot.delete_peoula', raise_exception=True)(supprimerPeoula), name='supprimer'),
-)
+]
--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import *
+from django.conf.urls import include, url
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -10,12 +10,12 @@ from JJL.views import login_view, logout_view, home
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', home, name='login'),
     url(r'^login/$', login_view, name='validate'),
     url(r'^logout/$', logout_view, name='logout'),
-    (r'^peoulot/', include('JJL.Peoulot.urls')),
+    url(r'^peoulot/', include('JJL.Peoulot.urls')),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls))
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^admin/', include(admin.site.urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
